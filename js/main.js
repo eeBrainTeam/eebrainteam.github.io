0266e5feab6452f65207587a2118ccd6e803b61d
Ömer Ayyıldız <oayyildiz416@gmail.com>
2020-05-23 18:47:04 +0300

    Change hover and photos

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@
     [ Simple slide100 ]*/
 
     $('.simpleslide100').each(function(){
-        var delay = 7000;
+        var delay = 10000;
         var speed = 1000;
         var itemSlide = $(this).find('.simpleslide100-item');
         var nowSlide = 0;
